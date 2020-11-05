9355afbc70e2ea912bd7f321043340ff5fa0127e
Shubham Bundela <sbundela88@gmail.com>
2020-11-05 17:41:50 +0530

    Changed Pubspec.yaml

---
+++ lib/constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const cIntroSliderBg = Color(0xfff9f9f9);
+
+const cPrimaryColor = Color(0xff5739bd);
+
+const cHeadStyle = TextStyle(
+  fontWeight: FontWeight.w600,
+  fontSize: 24,
+);
+
+const cTextStyle = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontSize: 16,
+  color: Colors.grey,
+  letterSpacing: 1.0,
+);
+
+const cFormFieldStyle = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontSize: 18.0,
+  letterSpacing: 1.0,
+);
