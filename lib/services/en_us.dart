cbe32f6e88e47b8a960397b32ad270dcd952c6a2
Shubham Bundela <sbundela88@gmail.com>
2020-11-21 18:34:10 +0530

    Fixed Code Formatting

--- lib/services/en_us.dart
+++ lib/services/en_us.dart
@@ -1,30 +1,23 @@
-const Map<String , String > enUS={
-  "selectLang":"Language Selection",
-  "selectLangBody":"Choose your preferred language",
-  "saveLang" :"Save",
-
-  "permissionTitle":"Give Permissions",
-  "permissionButton":"Allow",
+const Map<String, String> enUS = {
+  "selectLang": "Language Selection",
+  "selectLangBody": "Choose your preferred language",
+  "saveLang": "Save",
+  "permissionTitle": "Give Permissions",
+  "permissionButton": "Allow",
   "sliderTitle1": "QR and Instant Payments",
   "sliderTitle2": "Bill Payments",
-  "sliderTitle3":  "Security",
-  "sliderTitle4":  "Withdraw Funds",
-
-
+  "sliderTitle3": "Security",
+  "sliderTitle4": "Withdraw Funds",
   "sliderButton1": "Get Started",
   "sliderButton2": "Start Paying Bills",
   "sliderButton3": "Cool!",
   "sliderButton4": "Let's begin",
-
   "skipLogin": "skip to login",
-
   "registrationTitle": "Registration",
   "registrationBody": "Enter your mobile number to receive verification code",
   "phoneHint": "Enter Phone Number",
   "phoneEmpty": "Please Enter Valid phone Number",
   "otpButton": "Get OTP",
-
-
   "verifyNumberTitle": "Verify your number",
   "verifyNumberBody": "Enter a 6 digit number sent to",
   "verifyResendCode": "Re Send Code",
@@ -34,9 +27,7 @@ const Map<String , String > enUS={
   "incorrectOTPContent": "The OTP you've entered is wrong",
   "incorrectButton": "Ok",
   "otpVerifyButton": "Verify",
-
   "mobileVerifiedDesc": "Your mobile number has been verified successfully",
-
   "completeProfileTitle": "Complete Profile",
   "completeProfileName": "Enter Your Name",
   "incorretProfileName": "Name length must be valid",
@@ -44,21 +35,17 @@ const Map<String , String > enUS={
   "incorrectProfileEmail": "Enter Valid Email",
   "completeProfileReferral": "Referral Code",
   "completeProfileNext": "Next",
-
   "setPasscodeTitle": "Set Mobile Passcode",
   "setPasscodeBody": "Enter a 4 digit passcode to secure your account",
   "PasscodeDigitsOnly": "Enter Digits Only",
   "incorrectSetPasscode": "Please Enter Valid passcode",
   "setPasscodeButton": "Confirm Passcode",
-
   "confirmPasscodeTitle": "Confirm Mobile Passcode",
   "confirmPasscodeBody": "Please Re Enter your 4 digit passcode",
   "incorrectConfirmPasscode": "Passcode doesn't match, Please Check!!",
   "confirmPasscodeButton": "Set Passcode",
-
   "passcodeVerifedDesc": "Your passcode has been set successfully",
-
-  "secureTitle":"Secure your Payso",
-  "secureControllerBody":"Scan your finger to authenticate",
-  "SecureButton":"Check with Biometrics"
+  "secureTitle": "Secure your Payso",
+  "secureControllerBody": "Scan your finger to authenticate",
+  "SecureButton": "Check with Biometrics"
 };
