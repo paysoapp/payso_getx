a7c0a963505158fe1a3233ea15001a1b9a5da757
Naresh Khatri <nareshkhatri025@gmail.com>
2020-11-24 04:39:00 -0800

    localization screen added

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:payso/screens/LanguageSelection/select_language.dart';
 
 import 'screens/Splash/splash_screen.dart';
+import 'services/localization_service.dart';
+import 'services/localization_service.dart';
+import 'services/localization_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,9 +20,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      locale: LocalizationService.locale,
+      fallbackLocale: LocalizationService.fallbackLocale,
+      translations: LocalizationService(),
       debugShowCheckedModeBanner: false,
       title: 'PaySo App',
-      home: SplashScreen(),
+      home: SelectLanguage(),
     );
   }
 }
