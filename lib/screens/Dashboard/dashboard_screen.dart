86891197654a0a41d135c4ee6089a7b4acf36bd2
amitgeed <amitkumargeed@gmail.com>
2020-12-24 19:07:11 +0530

    fixed dashboard UI and resolve conflicts

--- lib/screens/Dashboard/dashboard_screen.dart
+++ lib/screens/Dashboard/dashboard_screen.dart
@@ -12,7 +12,7 @@ import '../../constants.dart';
 
 // ignore: must_be_immutable
 class DashboardScreen extends StatelessWidget {
-int index=0;
+  int index = 0;
   RoutesController routesController = Get.find();
   @override
   Widget build(BuildContext context) {
@@ -29,7 +29,9 @@ int index=0;
           IconButton(
               icon: Icon(Icons.person, color: Colors.white),
               tooltip: 'settings icon',
-              onPressed: null),
+              onPressed: () {
+                Get.to(MyProfileScreen());
+              }),
         ],
         backgroundColor: cPrimaryColor,
         elevation: 30.0,
@@ -44,69 +46,6 @@ int index=0;
           ],
         ),
       ),
-
-      bottomNavigationBar: BottomNavigationBar(
-        elevation: 0.0,
-        fixedColor: Colors.black,
-        currentIndex: index,
-        onTap: (int index){
-          index=this.index;
-          if(index==0)
-            {
-              Get.to(MyProfileScreen());
-            }
-        },
-        type: BottomNavigationBarType.fixed,
-        selectedFontSize: 14,
-        unselectedFontSize: 14,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              color: cPrimaryColor,
-            ),
-            label: 'Home',
-
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.lock,
-              color: cPrimaryColor,
-            ),
-            label: 'Store',
-          ),
-          BottomNavigationBarItem(
-            icon: Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: cPrimaryColor,
-              ),
-              child: Icon(
-                Icons.qr_code_scanner,
-                color: Colors.white,
-                size: 40,
-              ),
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.account_balance_wallet,
-              color: cPrimaryColor,
-            ),
-            label: 'Wallet',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.format_list_bulleted,
-              color: cPrimaryColor,
-            ),
-            label: 'History',
-          ),
-        ],
-      ),
-
     );
   }
 }
